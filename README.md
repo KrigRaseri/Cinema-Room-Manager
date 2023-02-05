169b655c302b279c75d0542fbc1d41e589e3ef40
Krig Raseri <krigraseri@gmail.com>
2023-02-04 23:00:07 -0600

    Add cinema seating grid
    This adds a 8x7 grid that prints out to represent cinema seating.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cinema Room Manager
  JetBrains Project Cinema Room Manager
+
+This is a JetBrains academy project to practice various Java skills, as well as git, github, and practicing proper commits and documentation.
---
+++ src/Cinema.java
@@ -0,0 +1,11 @@
+public class Cinema {
+    public static void main(String[] args) {
+        //Prints a 8x7 grid representing seats in a cinema.
+        System.out.println("Cinema:");
+        System.out.println(" 1 2 3 4 5 6 7 8");
+        for (int i = 1; i < 8; i++) {
+            System.out.println(i + " S S S S S S S S");
+
+        }
+    }
+}
