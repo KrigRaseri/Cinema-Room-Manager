6020a9cc9dfde4823ee0ed519fa426d34e177585
Krig Raseri <krigraseri@gmail.com>
2023-02-07 22:31:33 -0600

    Update cinema class
    Updates the cinema class to add in support for the user to choose the statistic class, as well as minor changes to remove all useless print statements and the like.

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,8 +1,5 @@
 package cinema;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Cinema {
@@ -10,16 +7,20 @@ public class Cinema {
         //Sets up an array for seating and asks for user input for rows and seats.
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
-        int row = sc.nextInt();
+        final int row = sc.nextInt();
         System.out.print("Enter the number of seats in each row: ");
-        int seat = sc.nextInt();
+        final int seat = sc.nextInt();
         String[][] arr = new String[row][seat];
+
 
 
         //Method calls
         initialize(arr, row, seat);
 
+
     }
+
+
 
     /**
      * Initializes the program by calling createSeats to use the user chosen rows and seats to build the seating
@@ -32,11 +33,14 @@ public class Cinema {
     * */
     public static void initialize(String[][] array, int row, int seat) {
         createSeats(array, row, seat);
+        int profits = 0;
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.println("""
                     1. Show the seats
                     2. Buy a ticket
+                    3. Statistics
                     0. Exit""");
 
             int input = sc.nextInt();
@@ -44,7 +48,8 @@ public class Cinema {
 
             switch (input) {
                 case 1 -> printSeats(array, row, seat);
-                case 2 -> ticketPrice(array, row, seat);
+                case 2 -> profits += ticketPrice(array, row, seat);
+                case 3 -> cinemaUtility.statistics(array, row, seat, profits);
                 default -> System.out.println("Try again");
             }
         }
@@ -52,36 +57,43 @@ public class Cinema {
 
     /**
      * Method to chose a seat based off of the initial seating arrangement size of rows x seats. Outputs the price
-     * of the chosen seat and updates the initial array to replace the "S" in the chosen position with a "B"
+     * of the chosen seat and updates the initial array to replace the "S" in the chosen position with a "B" for bought.
      *
      * @param array represents the initial seating array created in main.
      * @param row represents the number of rows chosen in main.
      * @param seats represents the number of seats in a row chosen in main.
      * */
-    public static void ticketPrice(String[][] array, int row, int seats) {
+    public static int ticketPrice(String[][] array, int row, int seats) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a row number: ");
         int rowBuy = sc.nextInt();
         System.out.print("Enter a seat number in that row: ");
         int seatBuy = sc.nextInt();
+        int profits = 0;
 
 
             //If total amount of seats is less than or equal to 60 then seats are 10$ each.
             if (row * seats <= 60) {
                 System.out.println("Ticket price: $10");
                 array[rowBuy-1][seatBuy-1] = "B";
+                profits += 10;
             }
 
             //More than 60 seats and even, then front half 10$ per seat, back half 8$ per seat.
             else if (row * seats > 60 && row % 2 == 0) {
                 if (rowBuy <= row/2){
                     System.out.println("Ticket price: $10");
+                    profits += 10;
+
                 }
 
                 else {
                     System.out.println("Ticket price: $8");
+                    profits += 8;
+
                 }
                 array[rowBuy-1][seatBuy-1] = "B";
+
 
 
             }
@@ -90,17 +102,18 @@ public class Cinema {
             else if (row * seats > 60 && row % 2 != 0) {
                 if (rowBuy <= row/2) {
                     System.out.println("Ticket price: $10");
+                    profits += 10;
                 }
 
                 else {
                     System.out.println("Ticket price: $8");
-
+                    profits += 8;
                 }
                 array[rowBuy-1][seatBuy-1] = "B";
             }
 
             printSeats(array, row, seats);
-
+            return profits;
     }
 
     /**
@@ -146,48 +159,33 @@ public class Cinema {
     /**
     * Method that calculates the total income if all seats are purchased based on the number of rows and seats.
     */
-    public static void ticketIncome() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int rows;
-        int nSeats;
-
-
-        try {
-            System.out.print("Enter the number of rows: ");
-            rows = Integer.parseInt(reader.readLine());
-
-            System.out.print("Enter the number of seats in each row: ");
-            nSeats = Integer.parseInt(reader.readLine());
-
-
+    public static String ticketIncome(int rows, int seats) {
+            String result = "";
 
             //If total amount of seats is less than or equal to 60 then seats are 10$ each.
-            if (rows * nSeats <= 60) {
-                System.out.println("Total income:");
-                System.out.println("$" + rows * nSeats * 10);
+            if (rows * seats <= 60) {
+                result = ("$" + (rows * seats * 10));
             }
 
             //More than 60 seats and even, then front half 10$ per seat, back half 8$ per seat.
             else if (rows % 2 == 0) {
-                System.out.println("Total income:");
-                int total = rows/2 * nSeats;
-                System.out.println("$" + ((total * 10) + (total * 8)));
+                int total = rows/2 * seats;
+                result = ("$" + ((total * 10) + (total * 8)));
 
             }
 
             //More than 60 seats and odd, then the even front half 10$ and the odd back half 8$.
             else {
-                System.out.println("Total income:");
-                int totalFront = (rows/2 * nSeats) * 10;
-                int totalBack = ((rows/2 + 1) * nSeats) * 8;
-                System.out.println("$" + (totalFront + totalBack));
+                int totalFront = (rows/2 * seats) * 10;
+                int totalBack = ((rows/2 + 1) * seats) * 8;
+                result = ("$" + (totalFront + totalBack));
 
             }
+
+            return result;
         }
 
-        catch (IOException e) {
-            e.printStackTrace();
-        }
 
-    }
+
+
 }
